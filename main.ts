bceba7abd7853c786be40fac1c087c987eb1a956
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-11-03 14:39:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,7 +22,18 @@ basic.forever(function () {
         basic.pause(100)
         led.toggle(0, 4)
     }
-    for (let index = 0; index < 3; index++) {
-    	
+    for (let index = 0; index < 6; index++) {
+        led.plot(2, 2)
+        led.plot(1, 1)
+        led.plot(3, 1)
+        led.plot(1, 3)
+        led.plot(3, 3)
+        basic.pause(100)
+        led.unplot(2, 2)
+        led.unplot(1, 1)
+        led.unplot(3, 1)
+        led.unplot(1, 3)
+        led.unplot(3, 3)
+        basic.pause(100)
     }
 })
